4a1d9ae82f2f5bfc427486d6919ffa8bedf8d2b6
Felipe Pires Iasbik <felipeiasbik@hotmail.com>
2023-04-27 14:39:42 -0300

    feat : logic to add a new user to the database with Joi validations

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import chalk from 'chalk';
+import router from './routes/index.routes.js';
 
 dotenv.config();
 const app = express();
 
 app 
 	.use(cors())
-	.use(express.json());
+	.use(express.json());	
+app.use(router);
 
 app.listen( process.env.PORT , () => {
 	console.log( `Server is running on ${chalk.green( `http://localhost:${process.env.PORT}` )}` );
