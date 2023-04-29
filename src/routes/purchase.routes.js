e952d233caacabf0bda0bc3528f828b8842da3e6
Taynara Pecorario <taynara_rp22@hotmail.com>
2023-04-29 15:38:58 -0300

    feat/ finishing purchases methods

--- src/routes/purchase.routes.js
+++ src/routes/purchase.routes.js
@@ -1,15 +1,13 @@
 import { Router } from 'express';
 import { authValidation } from '../middlewares/authValidation.middleware.js';
 import { validateSchema } from '../middlewares/validateSchema.middleware.js';
+import { newPurchase } from '../controllers/purchase.controller.js';
 import purchaseSchema from '../schemas/purchase.schema.js';
 
 const purchaseRouter = Router();
 
 purchaseRouter.use(authValidation);
 
-purchaseRouter.post('/purchase', validateSchema(purchaseSchema), (req, res) => {
-	console.log('Oi: ', req.body);
-	return res.send(req.body);}
-);
+purchaseRouter.post('/purchase', validateSchema(purchaseSchema), newPurchase);
 
 export default purchaseRouter;
